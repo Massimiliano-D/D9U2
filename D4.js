f74e54e015551b9f0470b5cf3b2fc0dc72cd3ccb
Massimiliano-D <massimiliano.dorsi96@gmail.com>
2023-07-06 16:55:23 +0200

    D9U2

---
+++ D4.js
@@ -0,0 +1,123 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+*/
+
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+function area(l1, l2) {
+  const A = l1 * l2;
+  return A;
+}
+console.log(area(92, 63));
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+function crazySum(n1, n2) {
+  let sum = 0;
+  if (n1 === n2) {
+    sum = (n1 + n2) * 3;
+  } else {
+    sum = n1 + n2;
+  }
+  return sum;
+}
+console.log(crazySum(2, 2));
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+function crazyDiff(n3) {
+  let diff = 0;
+  if (n3 > 19) {
+    diff = (n3 - 19) * 3;
+  } else {
+    diff = n3 - 19;
+  }
+  return diff;
+}
+console.log(crazyDiff(30));
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+function boundary(n) {
+  if ((n > 20 && n <= 100) || n === 400) {
+    n = true;
+  } else n = false;
+  return n;
+}
+console.log(boundary(20));
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+ 
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(n4) {
+  if (n4 % 3 === 0 && n4 % 7 === 0) {
+    n4 = "divisibile per 3 e per 7";
+  } else if (n4 % 3 === 0) {
+    n4 = "divisibile per 3";
+  } else if (n4 % 7 === 0) {
+    n4 = "divisibile per 7";
+  } else n4 = "RITENTA!";
+  return n4;
+}
+console.log(check3and7(9));
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+function reverseString(EPICODE) {
+  const myString = "Fun Fun Function";
+  EPICODE = myString.split("");
+}
+console.log(reverseString(EPICODE));
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
